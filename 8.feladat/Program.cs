9af78d9ada2a99f09aa029723fd54e764c5ec3c9
kopasz.bela <kopaszbeci@gmail.com>
2026-01-16 12:51:35 +0100

    9-ig megoldva

---
+++ 8.feladat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.feladat
+{
+    internal class Program
+    {
+        static long Faktorial(int szam) 
+        {
+            long fakt = 1;
+            for (int i = 1; i <= szam; i++)
+            {
+                fakt = fakt * i;
+            }
+            return fakt;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Adj meg egy számot: ");
+            int.TryParse(Console.ReadLine(),out int szam);
+            Console.WriteLine($"{szam}! = {Faktorial(szam)}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 9.feladat/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.feladat
+{
+    internal class Program
+    {
+        static bool PrimE(int szam)
+        {
+            int osztokSzama = 0;
+            for (int i = 1; i <= szam; i++)
+            {
+                if (szam%i==0)
+                {
+                    osztokSzama++;
+                }
+            }
+            if (osztokSzama==2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Kérek egy számot: ");
+                int szam = int.Parse(Console.ReadLine());
+                if (PrimE(szam) == true)
+                {
+                    Console.WriteLine("A szám prím!");
+                }
+                else
+                {
+                    Console.WriteLine("A szám nem prím!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba!" + ex.Message);
+            }
+
+            
+            Console.ReadKey();
+        }
+    }
+}
